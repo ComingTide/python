23fdc31fd99da72f0249e4e2ec743ce3a2686dbc
Ilya Baranov <mrfirebrand@icloud.com>
2021-11-16 23:06:57 +0300

    task_2

---
+++ 2_3.py
@@ -0,0 +1,17 @@
+class ItemDiscount:
+    def __init__(self, name, price):
+        self.__name = name
+        self.__price = price
+
+
+class ItemDiscountReport(ItemDiscount):
+    def __init__(self, name, price):
+        self.__name = name
+        self.__price = price
+
+    def get_parent_data(self):
+        print(self.__name, self.__price)
+
+
+child = ItemDiscountReport('sale', 10)
+print(child.get_parent_data())
